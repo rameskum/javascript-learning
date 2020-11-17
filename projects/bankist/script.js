82869e2f015dec1ccebfa9dfbec760bcd86f332f
rameskum <connecting.ramesh@gmail.com>
2020-11-17 18:15:00 +0530

    adding sorting functionality

--- projects/bankist/script.js
+++ projects/bankist/script.js
@@ -63,9 +63,12 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
 	containerMovements.innerHTML = '';
-	movements.forEach(function (mov, i) {
+
+	const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+	movs.forEach(function (mov, i) {
 		const type = mov > 0 ? 'deposit' : 'withdrawal';
 		const html = `
 		<div class="movements__row">
@@ -196,4 +199,11 @@ btnLoan.addEventListener('click', function (ele) {
 		updateUi(currentAccount);
 	}
 	inputLoanAmount.value = null;
+});
+
+let sort = false;
+btnSort.addEventListener('click', function (e) {
+	e.preventDefault();
+	displayMovements(currentAccount.movements, !sort);
+	sort = !sort;
 });
