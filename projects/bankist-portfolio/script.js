737128a44f49551ec2c37968b3ae12d6fee0baef
rameskum <connecting.ramesh@gmail.com>
2020-11-29 18:04:03 +0530

    spell correct

--- projects/bankist-portfolio/script.js
+++ projects/bankist-portfolio/script.js
@@ -6,7 +6,7 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnOpenModals = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function () {
 	modal.classList.remove('hidden');
@@ -18,7 +18,7 @@ const closeModal = function () {
 	overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+btnOpenModals.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
